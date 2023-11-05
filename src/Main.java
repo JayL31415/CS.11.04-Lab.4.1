e45c9a979b9307256796800ec3f32aaee276728e
JayL31415 <143620690+JayL31415@users.noreply.github.com>
2023-11-05 20:50:09 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.util.jar.JarOutputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hey");
+
     }
     // 1. parenthesesCheck
     public static boolean parenthesesCheck(String str) {
